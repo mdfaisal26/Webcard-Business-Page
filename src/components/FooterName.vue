bbd68380ea873ba66a123fe9c7750d4ab301ee40
hassan <hassaninbox17@gmail.com>
2023-11-17 22:36:10 +0530

    header and footer

---
+++ src/components/FooterName.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="footerContainer">Hello World</div>
+</template>
+  
+<script>
+export default {
+    name: 'FooterName'
+}
+</script>
+
+<style>
+#footerContainer {
+    background-color: rgb(17, 133, 133);
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="homePageContainer">
+        <HeaderBar class="header" />
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos inventore nobis et? Ullam odit laboriosam ea ex
+            sapiente officiis, debitis dolorem dolore quaerat illo culpa, cum maxime neque architecto quis.Body scrollable
+            contentLorem ipsum dolor sit amet consectetur adipisicing elit. Eos inventore nobis et? Ullam odit laboriosam ea
+            ex
+            sapiente officiis, debitis dolorem dolore quaerat illo culpa, cum maxime neque architecto quis.Body scrollable
+            contentLorem ipsum dolor sit amet consectetur adipisicing elit. Eos inventore nobis et? Ullam odit laboriosam ea
+            ex
+            sapiente officiis, debitis dolorem dolore quaerat illo culpa, cum maxime neque architecto quis.Body scrollable
+            contentLorem ipsum dolor sit amet consectetur adipisicing elit. Eos inventore nobis et? Ullam odit laboriosam ea
+            ex
+            sapiente officiis, debitis dolorem dolore quaerat illo culpa, cum maxime neque architecto quis.Body scrollable
+            contentLorem ipsum dolor sit amet consectetur adipisicing elit. Eos inventore nobis et? Ullam odit laboriosam ea
+            ex
+            sapiente officiis, debitis dolorem dolore quaerat illo culpa, cum maxime neque architecto quis.Body scrollable
+            contentLorem ipsum dolor sit amet consectetur adipisicing elit. Eos inventore nobis et? Ullam odit laboriosam ea
+            ex
+            sapiente officiis, debitis dolorem dolore quaerat illo culpa, cum maxime neque architecto quis.Body scrollable
+            content</div>
+        <FooterName />
+    </div>
+</template>
+  
+<script>
+import HeaderBar from './HeaderBar.vue'
+import FooterName from './FooterName.vue'
+
+export default {
+    name: 'HomePage',
+    components: {
+        HeaderBar,
+        FooterName
+    }
+}
+</script>
+
+<style>
+.header {
+    background-color: rgb(122, 255, 255);
+    position: sticky;
+    top: 0;
+    /* width: 100%; */
+}
+</style>
